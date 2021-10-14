5dc3481d13612db3652805fce3f1fff2ce7c7d64
FEI YUAN <fei.yuan@bcm.edu>
2021-10-14 14:05:59 -0500

    Bumped to version 1.0.19

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.md') as f:
 
 setup(
     name='eclip-peak',
-    version='1.0.18',
+    version='1.0.19',
     description='Pipeline for using IDR to identify a set of reproducible peaks given eClIP dataset with '
                 'two or three replicates.',
     long_description=long_description,
